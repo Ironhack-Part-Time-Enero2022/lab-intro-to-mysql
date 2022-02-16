ada76badf1bbf3694883de113080ebb065024270
Pablo <pablo.manzi.16@gmail.com>
2022-02-16 12:14:59 +0100

    Lab SQL

---
+++ your-code/update.sql
@@ -0,0 +1,11 @@
+UPDATE clientes
+SET Email = 'ppicasso@gmail.com'
+WHERE idClientes = '10001'
+
+UPDATE clientes
+SET Email = 'lincoln@us.gov'
+WHERE idClientes '20001'
+
+UPDATE clientes
+SET Email = 'hello@napoleon.me'
+WHERE idClientes = '30001'
