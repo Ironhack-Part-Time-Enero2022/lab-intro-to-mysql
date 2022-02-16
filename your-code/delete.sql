f00f8a7b8f9c3ce909896b62e14e9fc167508eae
EduardoArroyoGil <eduardoarroyogil@gmail.com>
2022-02-16 04:06:56 +0100

    lab-intro-to-sql Eduardo Arroyo
    con imagen incluida jeje

---
+++ your-code/delete.sql
@@ -0,0 +1,5 @@
+USE lab_sql_eduardoarroyo;
+delete from `lab_sql_eduardoarroyo`.`cars`
+where `lab_sql_eduardoarroyo`.`cars`.`ID`= 4;
+
+
---
+++ your-code/update.sql
@@ -0,0 +1,21 @@
+USE lab_sql_eduardoarroyo;
+
+UPDATE `lab_sql_eduardoarroyo`.`customers`
+SET
+`city` = "Miami"
+WHERE `name` = "Paige Turner";
+
+UPDATE `lab_sql_eduardoarroyo`.`customers`
+SET
+`email` = "ppicasso@gmail.com"
+WHERE `name` = "Pablo Picasso";
+
+UPDATE `lab_sql_eduardoarroyo`.`customers`
+SET
+`email` = "lincoln@us.gov"
+WHERE `name` = "Abraham Lincoln";
+
+UPDATE `lab_sql_eduardoarroyo`.`customers`
+SET
+`email` = "	hello@napoleon.me"
+WHERE `name` = "Napoléon Bonaparte"; 
